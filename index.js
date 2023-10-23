6d99e81c80676f383a043a8c9892040fd2efcec5
Yousef Sayed <yousefsayedd1@gmail.com>
2023-10-23 13:43:43 +0300

    remove unnecessary spaces

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-
 const select = document.querySelector('select');
-    const color = document.querySelector('input');
-    const colorsDiv = document.querySelector('#colors-Div');
-    const colorsNamesDiv = document.querySelector('#colors-names');
+const color = document.querySelector('input');
+const colorsDiv = document.querySelector('#colors-Div');
+const colorsNamesDiv = document.querySelector('#colors-names');
+
 document.querySelector('button').addEventListener('click', function (){
     fetch(`https://www.thecolorapi.com/scheme?hex=${color.value.slice(1)}&mode=${select.value}`)
     .then(res => res.json())
